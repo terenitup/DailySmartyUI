4bd15ba2b825628cc3125fb9363bdfa84416f97e
terenitup <terentaniuchi@gmail.com>
2018-08-23 14:40:12 -0600

    generated project

---
+++ src/components/app.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className='app'>
+        <div>
+          <div>
+            logo
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
